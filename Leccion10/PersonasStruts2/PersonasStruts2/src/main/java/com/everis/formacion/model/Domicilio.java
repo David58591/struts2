880bccdd396a1a9e36577d1d8240f282a4822775
David58591 <david.egea.daw@gmail.com>
2020-11-16 17:44:46 +0100

    Lección09,10,11 Struts2HibernateSpring

---
+++ Leccion10/PersonasStruts2/PersonasStruts2/src/main/java/com/everis/formacion/model/Domicilio.java
@@ -0,0 +1,12 @@
+package com.everis.formacion.model;
+
+import lombok.Data;
+
+@Data
+public class Domicilio {
+    private String calle;
+    private int numeroCalle;
+    private String pais;
+    
+    
+}
